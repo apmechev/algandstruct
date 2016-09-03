0dccf98601b7411c05fc6d6a14e84a3d0b9a3e20
Alexandar Mechev <apmechev@gmail.com>
2016-09-03 15:13:03 +0200

    updated logistic map to take parameters

--- c++/logisticmap.cpp
+++ c++/logisticmap.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
-#include <iomanip>
+#include <iomanip>      /*setprecision*/
+#include <stdlib.h>       /*atof*/
 
-int main(){
+/*
+ * either takes inputs from stdin or from cmdline:
+ * argv[1]=x_i
+ * argv[2]=R
+ * argv[3]=n->number of iterations
+ */
+int main(int argc, char* argv[]){
     long n,i;
     double r,xi,xj;
-    std::cout<<"I will calculate the logistic map for nth iteration"<<std::endl;
-    std::cout<<"What is n?";
-    std::cin>>n;
-    std::cout<<"What is R?"<<std::endl;
-    std::cin>>r;
-    std::cout<<"What is x0?"<<std::endl;
-    std::cin>>xi;
+    if (argc==1){
+        std::cout<<"I will calculate the logistic map for nth iteration"<<std::endl;
+        std::cout<<"What is n?";
+        std::cin>>n;
+        std::cout<<"What is R?"<<std::endl;
+        std::cin>>r;
+        std::cout<<"What is x0?"<<std::endl;
+        std::cin>>xi;
+    }
+    else{
+        xi=atof(argv[1]);
+        r=atof(argv[2]);
+        n=atof(argv[3]);
+    }
     i=0;
     while (i<n){
       xi=r*xi*(1.0-xi);
